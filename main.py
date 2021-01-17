aa42c103b26c1582d5153d44f00b1489a5b2fbd4
Baki <bakia@student.chalmers.se>
2021-01-17 05:17:58 +0100

    simple resource api added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+
 
 
 
@@ -8,8 +9,18 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 api = Api(app)
 
-db_url = 'mysql://root:''@217.0.0.1/movie'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+# db_url = 'mysql://root:''@127.0.0.1/movie'
 db = SQLAlchemy(app)
+
+class MovieModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    # def __repr__(self):
+    #     return f"Movies(id={id}, name={name})"
+
+db.create_all()
 
 helloworld_post_args = reqparse.RequestParser()
 helloworld_post_args.add_argument("name", type=str)
@@ -19,12 +30,25 @@ helloworld_post_args.add_argument("city", type=str)
 
 class HelloWorld(Resource):
     def get(self, name):
-        d = db.select("category")
-        return {"data": d}
+        return {"data": name}
     def post(self, name):
         return helloworld_post_args.parse_args()
 
+resource_fields = {
+    'id' : fields.Integer,
+    'name' : fields.String
+}
+
+class Movie(Resource):
+    @marshal_with(resource_fields)
+    def get(self, id):
+        result = MovieModel.query.filter_by(id=id).first()
+        return result
+    def post(self):
+        return id
+
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Movie, "/movies/<int:id>")
 
 
 if __name__ == "__main__":
